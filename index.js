88d78f17d11033f8065acfc2cbf427baeb8c6d2c
sukipop <slsean@pm.me>
2023-05-17 13:15:24 -0600

    attempting api key support

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+// Use body-parser middleware to handle json body in request
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const checkKey = (req, res, next) => {
+  const key = req.headers['x-api-key']
+
+  if (key === 'your_secret_key') {
+    next()
+  } else {
+    res.status(403).json({ error: 'Unauthorized' })
+  }
+}
+
+app.get('/', checkKey, (req, res) => {
   res.send('Hello World!')
 })
 
